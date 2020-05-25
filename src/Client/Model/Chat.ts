c2334a1068853856bdb77ee877f3e2292d98868f
Kevin Murphy <keggsmurph21@gmail.com>
2020-05-25 16:35:54 -0700

    Client/Model: Remove this unused dir
    It's still unclear to me exactly what we want our client-side
    abstractions to look like, but this wasn't used at the moment
    so I think it's reasonable to delete it.  If we end up needing
    it, then it's pretty easy to mkdir it again :^)

--- src/Client/Model/Chat.ts
+++
@@ -1,6 +0,0 @@
-import {Message} from "Interface/Chat";
-
-export function onMessage(data: Message) {
-    // FIXME: Implement
-    console.log("onMessage", data);
-}
--- src/Client/Model/Store.ts
+++
@@ -1,5 +0,0 @@
-export class Store<V> {
-    private storage: {[key: number]: V} = {};
-    get(key: number): V|null { return this.storage[key]; }
-    put(key: number, value: V): void { this.storage[key] = value; }
-}
